a7a2991c114064fe7329ac3f468cd0eddb30dc7b
noviek <cs.novie@gmail.com>
2015-06-03 18:21:56 +0700

    ganti bentuk

--- web/js/report.js
+++ web/js/report.js
@@ -1,113 +1,109 @@
 var chart = AmCharts.makeChart("chartdiv", {
-    "type": "serial",
-    "theme": "light",
-    "dataProvider": [{
-        "date": "2015-10-02",
-        "value": 5
-    }, {
-        "date": "2015-10-03",
-        "value": 15
-    }, {
-        "date": "2015-10-04",
-        "value": 13
-    }, {
-        "date": "2015-10-05",
-        "value": 17
-    }, {
-        "date": "2015-10-06",
-        "value": 15
-    }, {
-        "date": "2015-10-09",
-        "value": 19
-    }, {
-        "date": "2015-10-10",
-        "value": 21
-    }, {
-        "date": "2015-10-11",
-        "value": 20
-    }, {
-        "date": "2015-10-12",
-        "value": 20
-    }, {
-        "date": "2015-10-13",
-        "value": 19
-    }, {
-        "date": "2015-10-16",
-        "value": 25
-    }, {
-        "date": "2015-10-17",
-        "value": 24
-    }, {
-        "date": "2015-10-18",
-        "value": 26
-    }, {
-        "date": "2015-10-19",
-        "value": 27
-    }, {
-        "date": "2015-10-20",
-        "value": 25
-    }, {
-        "date": "2015-10-23",
-        "value": 29
-    }, {
-        "date": "2015-10-24",
-        "value": 28
-    }, {
-        "date": "2015-10-25",
-        "value": 30
-    }, {
-        "date": "2015-10-26",
-        "value": 72,
-        "customBullet": "/img/redstar.png"
-    }, {
-        "date": "2015-10-27",
-        "value": 43
-    }, {
-        "date": "2015-10-30",
-        "value": 31
-    }, {
-        "date": "2015-11-01",
-        "value": 30
-    }, {
-        "date": "2015-11-02",
-        "value": 29
-    }, {
-        "date": "2015-11-03",
-        "value": 27
-    }, {
-        "date": "2015-11-04",
-        "value": 26
-    }],
-    "valueAxes": [{
-        "axisAlpha": 0,
-        "dashLength": 4,
-        "position": "left"
-    }],
-    "graphs": [{
-        "bulletSize": 14,
-        "customBullet": "/img/star.png",
-        "customBulletField": "customBullet",
-        "valueField": "value",
-         "balloonText":"<div style='margin:10px; text-align:left;'><span style='font-size:13px'>[[category]]</span><br><span style='font-size:18px'>Value:[[value]]</span>",
-    }],
-    "marginTop": 20,
-    "marginRight": 70,
-    "marginLeft": 40,
-    "marginBottom": 20,
-    "chartCursor": {
-        "graphBulletSize": 1.5
-    },
-    "autoMargins": false,
-    "dataDateFormat": "YYYY-MM-DD",
-    "categoryField": "date",
-    "categoryAxis": {
-        "parseDates": true,
-        "axisAlpha": 0,
-        "gridAlpha": 0,
-        "inside": true,
-        "tickLength": 0
-    },
-    "export": {
-        "enabled": true
-    }
+	"type": "serial",
+	"categoryField": "date",
+	"dataDateFormat": "YYYY-MM",
+	"theme": "light",
+	"categoryAxis": {
+		"minPeriod": "MM",
+		"parseDates": true
+	},
+	"chartCursor": {
+		"categoryBalloonDateFormat": "MMM YYYY"
+	},
+	"trendLines": [],
+	"graphs": [
+		{
+			"bullet": "round",
+			"id": "AmGraph-1",
+			"title": "Expense",
+			"valueField": "column-1"
+		},
+		{
+			"bullet": "square",
+			"id": "AmGraph-2",
+			"title": "Income",
+			"valueField": "column-2"
+		}
+	],
+	"guides": [],
+	"valueAxes": [
+		{
+			"id": "ValueAxis-1",
+			"title": "Amount"
+		}
+	],
+	"allLabels": [],
+	"balloon": {},
+	"legend": {
+		"useGraphSettings": true
+	},
+	"titles": [
+		{
+			"id": "Title-1",
+			"size": 15,
+			"text": "This Year's Report"
+		}
+	],
+	"dataProvider": [
+		{
+			"date": "2014-01",
+			"column-1": 800,
+			"column-2": 500
+		},
+		{
+			"date": "2014-02",
+			"column-1": 600,
+			"column-2": 700
+		},
+		{
+			"date": "2014-03",
+			"column-1": 200,
+			"column-2": 300
+		},
+		{
+			"date": "2014-04",
+			"column-1": 100,
+			"column-2": 300
+		},
+		{
+			"date": "2014-05",
+			"column-1": 200,
+			"column-2": 100
+		},
+		{
+			"date": "2014-06",
+			"column-1": 300,
+			"column-2": 200
+		},
+		{
+			"date": "2014-07",
+			"column-1": 600,
+			"column-2": 800
+		},
+		{
+			"date": "2014-08",
+			"column-1": 300,
+			"column-2": 700
+		},
+		{
+			"date": "2014-09",
+			"column-1": 200,
+			"column-2": 300
+		},
+		{
+			"date": "2014-10",
+			"column-1": 100,
+			"column-2": 300
+		},
+		{
+			"date": "2014-11",
+			"column-1": 300,
+			"column-2": 300
+		},
+		{
+			"date": "2014-12",
+			"column-1": 700,
+			"column-2": 700
+		},
+	]
 });
